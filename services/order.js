89bfdef1938663f499f5861c45dd1599859a2036
kimone-9 <46690399+kimone-9@users.noreply.github.com>
2020-07-22 14:50:32 -0500

    Update order.js

--- services/order.js
+++ services/order.js
@@ -21,20 +21,7 @@ module.exports = class Order {
 
     switch (payload) {
       case "TRACK_ORDER":
-        response = Response.genQuickReply(i18n.__("order.prompt"), [
-          {
-            title: i18n.__("order.account"),
-            payload: "LINK_ORDER"
-          },
-          {
-            title: i18n.__("order.search"),
-            payload: "SEARCH_ORDER"
-          },
-          {
-            title: i18n.__("menu.help"),
-            payload: "CARE_ORDER"
-          }
-        ]);
+        response = Response.genText(i18n.__("order.prompt"));
         break;
 
       case "SEARCH_ORDER":
